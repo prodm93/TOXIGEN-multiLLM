51b537b7c4e3f64ef7aebe7eb11edb391bc7dbea
prodm93 <105621504+prodm93@users.noreply.github.com>
2024-11-14 18:22:46 +0700

    Update language_models.py

--- toxigen/language_models.py
+++ toxigen/language_models.py
@@ -1,6 +1,9 @@
 import requests
 import json
 from toxigen.alice import beam_search
+import os
+import google.generativeai as genai
+
 class GPT3(object):
     def __init__(self, endpoint_url, apikey):
         self.apikey = apikey
@@ -32,6 +35,48 @@ class GPT3(object):
     def from_prompt(self, prompt, topk=10, max_tokens=10):
         output = self.__call__(prompt, topk, max_tokens)
         return output["choices"][0]["text"]
+
+class GPT4(object):
+    def __init__(self, model_name, endpoint_url, apikey):
+        self.model = model_name
+        self.apikey = apikey
+        self.endpoint_url = endpoint_url
+
+    #def __call__(self, prompt, top_p=1, max_tokens=1):
+    def __call__(self, prompt, topk=1, max_tokens=1):
+        if not isinstance(prompt, list):
+            prompt = [prompt]
+        prompt = [p.replace("'", "").replace('"', "") for p in prompt]
+        client = openai.OpenAI(api_key=self.apikey)
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt[0]},
+        ]
+        response = client.chat.completions.create(
+            model=self.model,
+            #model="gpt-4o-mini",
+            messages=messages,
+            max_tokens=max_tokens,
+            temperature=0.9,
+            n=1,
+            stream=False,
+            logprobs=True,
+            top_logprobs=topk,
+            #top_p=top_p,
+            stop=["<|endoftext|>", "\\n", "\n"]
+        )
+
+        return response
+
+    def from_prompt(self, prompt, topk=10, max_tokens=10):
+        output = self.__call__(prompt, topk, max_tokens)
+        return output.choices[0].message.content
+    
+class LLaMA3(object):
+    pass
+
+class Gemini(object):
+    pass
 
 class ALICE(object):
     def __init__(self, language_model, classifier, mode, device="cpu"):
